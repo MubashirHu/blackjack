5894949b88c1939cd407c370dc57445b7e5ff2db
Mubashir <hussainmuby@gmail.com>
2023-04-06 20:26:51 -0600

    keeps count of the hand values

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import BlackJack
-
+import player
 def main():
+
     dealer = BlackJack.blackJackModerator()
-    
-    ##dealer.Entry()
-    print(len(dealer.deck))
-    dealer.dealCards()
-    print(len(dealer.deck))
-    print(dealer.handVal)
+    p1 = player.Player("Mubashir", 23)
+    p2 = player.Player("mike", 34)
+
+    dealer.playerCount.append(p1)
+    dealer.playerCount.append(p2)
+    dealer.shouldGameBegin()
+
+    #dealer.Entry()
     
     while(dealer.startedGame):
-        pass
+
+        # deal cards
+        dealer.dealCards()
+
+        # display the cards 
+        dealer.displayCards()
+        dealer.checkforHitorStand()
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -8,21 +8,4 @@ class Player:
         self.hand = []
         self.handVal = 0
 
-    def handValue(self):
-        i = 0
-        while(i<len(self.hand)):
-            temp = self.hand[i][1]
-
-            if temp == "J":
-                temp = 10
-            elif temp == "K":
-                temp = 10
-            elif temp == "Q":
-                temp = 10
-            elif temp == "A":
-                temp = 11
-            else:
-                temp = self.hand[i][1]
-
-            self.handVal += temp
-
+        
